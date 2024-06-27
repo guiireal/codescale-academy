8f1b2139c1105bd143fa3369bcdc07873cab5b82
guiireal <guii_real@hotmail.com>
2024-06-27 16:55:47 -0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
+use App\Livewire\Pages\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
+Route::get('/', Home::class)->name('home');
